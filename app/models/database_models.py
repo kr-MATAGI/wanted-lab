6c2ef025bd7128223d6b993d3f4e7f5e68f1d68d
kr-MATAGI <dev_matagi@kakao.com>
2025-06-19 17:44:02 +0900

    [Feature] Database Model 추가

---
+++ app/models/database_models.py
@@ -0,0 +1,57 @@
+from sqlalchemy import Column, Integer, Text, ForeignKey, ARRAY
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from app.utils.database import Base
+
+class CompanyID(Base):
+    """회사 ID"""
+    __tablename__ = "tbl_company_ids"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    company_id = Column(Integer, nullable=False)
+
+
+class Language(Base):
+    """사용하는 언어 정보"""
+    __tablename__ = "tbl_languages"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    language_type = Column(Text, nullable=False)
+
+
+class CompanyName(Base):
+    """회사 정보 모델"""
+    __tablename__ = "tbl_company_names"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Text, nullable=False)
+    company_id = Column(
+        Integer,
+        ForeignKey("tbl_company_ids.id"),
+        nullable=False,
+    )
+    language_id = Column(
+        Integer,
+        ForeignKey("tbl_languages.id"),
+        nullable=False,
+    )
+    
+    # 관계 설정
+    company = relationship("CompanyID", backref="company_names")
+    language = relationship("Language", backref="company_names")
+
+
+class Tag(Base):
+    """태그 정보 모델"""
+    __tablename__ = "tbl_tags"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tag_name = Column(Text, nullable=False)
+    company_id = Column(ARRAY(Integer), nullable=False)
+    language_id = Column(
+        Integer, 
+        ForeignKey("tbl_languages.id"),
+    )
+
+    # 관계 설정
+    language = relationship("Language", backref="tags")
